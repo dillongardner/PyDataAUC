9d6b399005e7cba2fa48771e02d3bd2948f0ea37
Dillon R. Gardner <dillon@gardner.fyi>
2022-06-16 11:01:33 +0100

    Init plots

--- generate_plots.py
+++ generate_plots.py
@@ -1,12 +1,38 @@
-from utils.roc_plotter import ROCPlotter
+from utils.roc_plotter import ROCPlotter, UtilityMatrix
 import matplotlib.pylab as plt
+import numpy as np
+
+utility_matrix = UtilityMatrix(100, -150, -10, -10, 0.5)
 
 plotter = ROCPlotter()
-plotter.plot_precision(0.5, 0.1)
-plotter.plot_precision(0.9, 0.1)
 plotter.show()
+plotter.fig.savefig('./blank.png')
+
+print(f'Slope of {utility_matrix} is {utility_matrix.slope:0.2f}')
+plotter = ROCPlotter()
+plotter.plot_iso_utility(utility_matrix, utility_matrix.utility_at_accept).\
+    plot_iso_utility(utility_matrix, utility_matrix.utility_at_reject).\
+    plot_iso_utility(utility_matrix, 5).\
+    plot_iso_utility(utility_matrix, 20)
+plotter.ax.set_title(f'Slope: {utility_matrix.slope:0.2f} ')
+plotter.show()
+#ax.title = f'Slope: {utility_matrix.slope}'
+plotter.fig.savefig('./iso_utility_1.png')
+plotter.fig.show()
+
+utility_matrix.prevalence = 0.6
+print(f'Slope of {utility_matrix} is {utility_matrix.slope:0.2f}')
+plotter = ROCPlotter()
+plotter.plot_iso_utility(utility_matrix, utility_matrix.utility_at_accept). \
+    plot_iso_utility(utility_matrix, utility_matrix.utility_at_reject). \
+    plot_iso_utility(utility_matrix, 10). \
+    plot_iso_utility(utility_matrix, 20)
+plotter.ax.set_title(f'Slope: {utility_matrix.slope:0.2f} ')
+plotter.show()
+#ax.title = f'Slope: {utility_matrix.slope}'
+plotter.fig.savefig('./iso_utility_2.png')
+plotter.fig.show()
+
 
 if __name__ == '__main__':
-    plt.show()
-    ROCPlotter()
-    plt.show()
+    pass
--- utils/roc_plotter.py
+++ utils/roc_plotter.py
@@ -12,30 +12,54 @@ class UtilityMatrix:
     u_fp: float
     u_tn: float
     u_fn: float
-    prevalence: float # must be between 0 and 1
+    prevalence: float  # must be between 0 and 1
 
     @property
-    def positivity(self):
+    def positivity(self) -> float:
+        """Positivity is number of positive examples divided by the number of negative examples"""
         return self.prevalence / (1 - self.prevalence)
 
     @property
-    def negativity(self):
+    def negativity(self) -> float:
+        """Negativity is number of negative examples divided by the total number of examples"""
         return 1 / self.positivity
 
     @property
-    def slope(self):
-        numerator = (self.u_tn - self.u_fn) * self.negativity
-        denominator = (self.u_tp - self.u_fn) * self.positivity
+    def negavence(self) -> float:
+        """Number of negative examples over total examples"""
+        return 1 - self.prevalence
+
+    @property
+    def slope(self) -> float:
+        numerator = (self.u_tn - self.u_fp) * self.negavence
+        denominator = (self.u_tp - self.u_fn) * self.prevalence
         return numerator / denominator
 
-    def intercept(self, utility: float):
-        # TODO double check this math
-        numerator = utility - self.u_fn * self.positivity - self.u_tn * self.negativity
-        denominator = (self.u_tp - self.u_fn) * self.positivity
+    def intercept(self, utility: float) -> float:
+        """
+        Returns the Intercept for a given utility
+        """
+        numerator = utility - self.u_fn * self.prevalence - self.u_tn * self.negavence
+        denominator = (self.u_tp - self.u_fn) * self.prevalence
         return numerator / denominator
 
-    def tpr_from_fpr_and_prevalence(self, fpr: float):
-        pass
+    def tpr_from_fpr_utility(self, fpr: float, utility: float) -> float:
+        """
+        Returns the TPR for a given FPR and Utility
+        """
+        return self.slope * fpr + self.intercept(utility)
+
+    @property
+    def utility_at_reject(self) -> float:
+        """Returns the utility value that occurs everything is reject
+        This is (0,0) in ROC space"""
+        return self.u_fn * self.prevalence + self.u_tn * self.negavence
+
+    @property
+    def utility_at_accept(self) -> float:
+        """Returns the utility value that occurs everything is accepted
+        This is (1,1) in ROC space"""
+        return (1 - self.slope)*(self.u_tp - self.u_fn) * self.prevalence + self.utility_at_reject
 
 
 class ROCPlotter:
@@ -48,7 +72,7 @@ class ROCPlotter:
         self.ax.set_xlabel('False Positive Rate')
         self.ax.set_ylabel('True Positive Rate')
 
-    def plot_precision(self, precision: float, prevalence: float = 0.5):
+    def plot_precision(self, precision: float, prevalence: float = 0.5, **kwargs):
         """Add line of a given precision.
         TPR = FPR / (r - precision)
         where r is the positivity ratio (P/N)
@@ -56,18 +80,21 @@ class ROCPlotter:
         r = prevalence / (1 - prevalence)
         fpr_values = np.linspace(0, 1, 5)
         tpr_values = fpr_values /  (r * (1 - precision))
-        self.ax.plot(fpr_values, tpr_values, label=f'Precision: {precision:0.2f}')
+        label = f'Precision: {precision:0.2f}\nPrevalence: {prevalence:0.2f}'
+        self.ax.plot(fpr_values, tpr_values, label=label, **kwargs)
+        return self
 
-    def plot_iso_utility(self, utility_matrix: UtilityMatrix, prevalence: float =  0.5):
+    def plot_iso_utility(self, utility_matrix: UtilityMatrix, utility: float, **kwargs):
         """Add line of iso-utility"""
-        r = prevalence / (1 - prevalence)
         fpr_values = np.linspace(0,1,5)
-
+        tpr_values =utility_matrix.tpr_from_fpr_utility(fpr_values, utility)
+        self.ax.plot(fpr_values, tpr_values, label=f'Utility: {utility:0.0f}')
+        return self
 
     def show(self):
         self.ax.legend()
         self.fig.tight_layout()
-        return self.fig, self.ax
+        return self
 
 
 
